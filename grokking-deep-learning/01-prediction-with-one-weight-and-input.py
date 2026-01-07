fa7fd952393cf18c3a3b074fdb3cd1e6153a4e63
Safiya Kirpichonak <sophiakirpichenok@gmail.com>
2026-01-07 05:33:37 +0100

    Updated imports, 01-prediction-with-one-weight-and-input.py

--- grokking-deep-learning/01-prediction-with-one-weight-and-input.py
+++ grokking-deep-learning/01-prediction-with-one-weight-and-input.py
@@ -1,10 +1,9 @@
-weight = 0.1
-
-def neural_network(input, weight):
-    prediction = input * weight
+def neural_network(input):
+    WEIGHT = 0.1
+    prediction = input * WEIGHT
     return prediction
 
 number_of_toes = [8.5, 9.5, 10, 9]
 for t in number_of_toes:
-    prediction = neural_network(t, weight)
+    prediction = neural_network(t)
     print(prediction)
--- grokking-deep-learning/vectors/__init__.py
+++ grokking-deep-learning/vectors/__init__.py
@@ -1,3 +1,9 @@
-from .index import weight_sum
+from .index import elementwise_multiplication, elementwise_addition, vector_sum, vector_average, weight_sum
 
-__all__ = ['weight_sum']
+__all__ = [
+    "elementwise_multiplication", 
+    "elementwise_addition", 
+    "vector_sum",
+    "vector_average",
+    "weight_sum"
+]
