85013c7744895ecb6247261d5cd47211bdb026b6
Safiya Kirpichonak <sophiakirpichenok@gmail.com>
2026-01-17 04:22:03 +0100

    Created 11-simplest-gradient-descent.py

---
+++ grokking-deep-learning/11-simplest-gradient-descent.py
@@ -0,0 +1,14 @@
+def neural_network(input, weight):
+    return input * weight
+
+INPUT = 0.5
+GOAL_PREDICTION = 0.8
+
+weight = 0.5
+for iteration in range(20):
+    prediction = neural_network(INPUT, weight)
+    direct_and_amount = (prediction - GOAL_PREDICTION) * INPUT
+    weight =  weight - direct_and_amount
+    print(f"Prediction: {str(round(prediction, 3))}")
+
+# Prediction: 0.798
