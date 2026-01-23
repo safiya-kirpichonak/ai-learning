303a490140b8d909ca0ecdafb38540f2f759024c
Safiya Kirpichonak <sophiakirpichenok@gmail.com>
2026-01-23 04:26:38 +0100

    Updated 13-gradient-descent-with-a-few-inputs.py

--- grokking-deep-learning/13-gradient-descent-with-a-few-inputs.py
+++ grokking-deep-learning/13-gradient-descent-with-a-few-inputs.py
@@ -1,30 +1,30 @@
 from vectors import weight_sum, number_multiplication
 
-# ERROR 
+
 def neural_network(input, weights):
     return weight_sum(input,  weights)
 
-
-number_of_toes = [8.5, 9.5, 9.9, 9.0]
-win_percentage = [0.65, 0.8, 0.8, 0.9]
-number_of_fans = [1.2, 1.3, 0.5, 1.0]
-win_or_lose_binary = [1, 1, 0, 1]
-
 ALPA = 0.1
-weights = [0.1, 0.2, -0.1]
-for i in range(len(number_of_toes)):
+WIN_OR_LOSE_BINARY = [1, 1, 0, 1]
+NUMBER_OF_TOES = [8.5, 9.5, 9.9, 9.0]
+NUMBER_OF_FANS = [1.2, 1.3, 0.5, 1.0]
+WIN_PERCENTAGE = [0.65, 0.8, 0.8, 0.9]
 
-    input = [number_of_toes[i], win_percentage[i], number_of_fans[i]]
+
+weights = [0.1, 0.2, -0.1]
+for i in range(len(NUMBER_OF_TOES)):
+
+    input = [NUMBER_OF_TOES[i], WIN_PERCENTAGE[i], NUMBER_OF_FANS[i]]
     prediction = neural_network(input, weights)
 
-    true = win_or_lose_binary[i]
+    true = WIN_OR_LOSE_BINARY[i]
     error = (prediction - true) ** 2
     delta = prediction - true
 
     weights_deltas = number_multiplication(delta, input)
 
     for j in range(len(weights)):
-        weights[i] = weights[i] - (ALPA * weights_deltas[i])
+        weights[j] = weights[j] - (ALPA * weights_deltas[j])
     
     print(f"Weights: {str(weights)}")
-    print(f"Weights Deltas: {str(weights_deltas)}")
+    print(f"Weights Deltas: {str(weights_deltas)}\n")
