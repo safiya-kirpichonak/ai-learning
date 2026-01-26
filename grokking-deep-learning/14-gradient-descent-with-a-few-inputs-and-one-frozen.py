6825223e5fcefa398549be5e71c90aabd8f1a133
Safiya Kirpichonak <sophiakirpichenok@gmail.com>
2026-01-26 04:42:32 +0100

    Updated 13-gradient-descent-with-a-few-inputs.py, created 14-gradient-descent-with-a-few-inputs-and-one-frozen.py

--- grokking-deep-learning/14-gradient-descent-with-a-few-inputs-and-one-frozen.py
+++ grokking-deep-learning/14-gradient-descent-with-a-few-inputs-and-one-frozen.py
@@ -6,23 +6,24 @@ def neural_network(input, weights):
 
 
 ALPA = 0.01
-WIN_OR_LOSE_BINARY = [1, 1, 0, 1]
-NUMBER_OF_TOES = [8.5, 9.5, 9.9, 9.0]
-NUMBER_OF_FANS = [1.2, 1.3, 0.5, 1.0]
-WIN_PERCENTAGE = [0.65, 0.8, 0.8, 0.9]
+WIN_OR_LOSE_BINARY = [1]
+NUMBER_OF_TOES = [8.5]
+NUMBER_OF_FANS = [1.2]
+WIN_PERCENTAGE = [0.65]
 
 weights = [0.1, 0.2, -0.1]
 for i in range(len(NUMBER_OF_TOES)):
     input = [NUMBER_OF_TOES[i], WIN_PERCENTAGE[i], NUMBER_OF_FANS[i]]
     true = WIN_OR_LOSE_BINARY[i]
 
-    for j in range(5):
+    for j in range(100):
         prediction = neural_network(input, weights)
 
         error = (prediction - true) ** 2
         delta = prediction - true
 
         weights_deltas = number_multiplication(delta, input)
+        weights_deltas[0] = 0
 
         for y in range(len(weights)):
             weights[y] = weights[y] - (ALPA * weights_deltas[y])
