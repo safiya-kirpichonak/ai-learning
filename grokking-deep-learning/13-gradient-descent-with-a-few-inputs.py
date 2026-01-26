f81701e5a1116c5fd850f0d6396890b0929e0ec7
Safiya Kirpichonak <sophiakirpichenok@gmail.com>
2026-01-26 04:04:40 +0100

    Updated 13-gradient-descent-with-a-few-inputs.py

--- grokking-deep-learning/13-gradient-descent-with-a-few-inputs.py
+++ grokking-deep-learning/13-gradient-descent-with-a-few-inputs.py
@@ -4,12 +4,12 @@ from vectors import weight_sum, number_multiplication
 def neural_network(input, weights):
     return weight_sum(input,  weights)
 
-ALPA = 0.1
+
+ALPA = 0.01
 WIN_OR_LOSE_BINARY = [1, 1, 0, 1]
 NUMBER_OF_TOES = [8.5, 9.5, 9.9, 9.0]
 NUMBER_OF_FANS = [1.2, 1.3, 0.5, 1.0]
 WIN_PERCENTAGE = [0.65, 0.8, 0.8, 0.9]
-
 
 weights = [0.1, 0.2, -0.1]
 for i in range(len(NUMBER_OF_TOES)):
@@ -26,5 +26,6 @@ for i in range(len(NUMBER_OF_TOES)):
     for j in range(len(weights)):
         weights[j] = weights[j] - (ALPA * weights_deltas[j])
     
+    print(f"Prediction: {prediction}; True: {true}; Error: {error}")
     print(f"Weights: {str(weights)}")
     print(f"Weights Deltas: {str(weights_deltas)}\n")
