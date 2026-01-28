d8d83f2b0db655c8d25289f6bfedcd3627b565ca
Safiya Kirpichonak <sophiakirpichenok@gmail.com>
2026-01-28 13:28:26 +0100

    Completed 15-gradient-descent-with-a-few-outputs.py

--- grokking-deep-learning/15-gradient-descent-with-a-few-outputs.py
+++ grokking-deep-learning/15-gradient-descent-with-a-few-outputs.py
@@ -1,14 +1,14 @@
-from vectors import elementwise_multiplication, number_multiplication
+from vectors import number_multiplication
 
 
 def neural_network(input, weights):
-    return elementwise_multiplication(input, weights)
+    return number_multiplication(input, weights)
 
 ALPHA = 0.1
 
+hurt = [0.1, 0.0, 0.0, 0.1]
 win = [1, 1, 0, 1]
 sad = [0.1, 0.0, 0.1, 0.2]
-hurt = [0.1, 0.0, 0.0, 0.1]
 
 wlrec = [0.65, 1.0, 1.0, 0.9]
 
@@ -28,12 +28,14 @@ for i in range(len(win)):
         
         weight_deltas = number_multiplication(input, weights)
 
-        for y in range(len(weights)):
-            weights[i] = weights[i] - (weight_deltas[i] * ALPHA)
+        for x in range(len(weights)):
+            weights[x] = weights[x] - (weight_deltas[x] * ALPHA)
 
         print(f"Weights: {str(weights)}")
         print(f"Weights Deltas: {str(weight_deltas)}")
-        print(f"Prediction: {str(prediction)}; True: {true}")
+        print(f"Prediction: {str(prediction)}; True: {true}\n")
+    
+    break
 
 
 
